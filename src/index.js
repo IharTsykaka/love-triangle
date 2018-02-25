694a6f9243085263c6f137a065f04e7982a0a6f2
IharTsykaka <35345292+IharTsykaka@users.noreply.github.com>
2018-02-26 01:19:45 +0300

    task 02

--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-   var n = preferences.length;
-  var val = 1;
-  for (var i=1; i<n; i++) {
-    val *= i;
-  }
-  return val;
-};
+    var sum = 0;
+    for (var i = 0; i<=preferences.length; i++){
+        var l = preferences[i] - 1;
+        if(l != i){
+            l = preferences[l] - 1;
+            if(preferences[l] - 1 == i) {
+                sum++;
+                preferences[i] = 0;
+            }
+        }
+    }
+    return sum;
+}

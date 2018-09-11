062d2322facb290389ae0f452b9dfeb1c73e6dd3
IharTsykaka <35345292+IharTsykaka@users.noreply.github.com>
2018-09-12 00:39:10 +0300

    twelveSemptemberFirst

--- src/index.js
+++ src/index.js
@@ -3,16 +3,5 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-    var sum = 0;
-    for (var i = 0; i<=preferences.length; i++){
-        var l = preferences[i] - 1;
-        if(l != i){
-            l = preferences[l] - 1;
-            if(preferences[l] - 1 == i) {
-                sum++;
-                preferences[i] = 0;
-            }
-        }
-    }
-    return sum;
+    
 }

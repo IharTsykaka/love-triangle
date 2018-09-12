5fd0a7d997334dca0ca18075c0761e9528942f16
IharTsykala <Sudja09@bk.ru>
2018-09-12 14:42:19 +0300

    twelveSentemberSolution

--- src/index.js
+++ src/index.js
@@ -4,4 +4,22 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
     
+    let count = 0;
+
+    for (let i = 0; i < preferences.length; i++) {
+
+        if (preferences[i] !== undefined) {
+            let j = preferences[i] - 1;
+            let k = preferences[j] - 1;
+            if (preferences[k] - 1 === i && i !== j && j !== k) {
+                count++;
+                preferences[i] = undefined;
+                preferences[j] = undefined;
+                preferences[k] = undefined; 
+            }
+        }
+        
+    }
+    return count;
 }
+
